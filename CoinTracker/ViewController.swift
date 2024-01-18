b0f00e0a27357a1f2d8610af0de39480dd9da8fb
Bernardo Lorenzini <lorenzinibernardo@gmail.com>
2024-01-18 00:49:19 -0300

    getting json information from api and displaying it on the ios

--- CoinTracker/ViewController.swift
+++ CoinTracker/ViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var euroPrice: UILabel!
+    @IBOutlet weak var btcPrice: UILabel!
+    
+    @IBOutlet weak var poundPrice: UILabel!
+    
+    @IBOutlet weak var realPrice: UILabel!
+    @IBOutlet weak var lastUpdatedPrice: UILabel!
+    
+    let urlString = "http://api.coingecko.com/api/v3/exchange_rates"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
